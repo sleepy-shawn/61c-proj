3408501526fc83500cabef90fb0ea23283548462
ggshawn <geshuai@nwafu.edu.cn>
2024-11-28 21:37:08 +0800

    read matrix finished! congratulationsgit add .git add .!

--- proj2/src/matmul.s
+++ proj2/src/matmul.s
@@ -26,38 +26,96 @@
 matmul:
 
     # Error checks
+    li t0 1
+    # dimensions do not make sense
+    blt a1 t0 exception
+    blt a2 t0 exception
+    blt a4 t0 exception
+    blt a5 t0 exception
+    # dimensions do not match
+    bne a2 a4 exception
 
 
     # Prologue
+    addi sp, sp, -32
+    sw s0, 0(sp) 
+    sw s1, 4(sp) 
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp) 
+    sw s6, 24(sp)
+    sw s7, 28(sp)
+    
+    li s0, 0 # set the begin index of outer loop
+    li s1, 0 # set the begin index of inner loop
+    mv s2, a1 # save the row num of m0
+    mv s3, a5 # save the column num of m1
+    mv s4, a0 # save the pointer of m0
+    mv s5, a3 # save the pointer of m1
+    mv s6, a2 # save the column num of m0
+    mv s7, a6 # save the pointer of the result matrix
+    
 
-
+    # for better simplified code
+    addi s7, s7, -4
+    
 outer_loop_start:
-
-
-
-
+    bge s0, s2, outer_loop_end    
+   # auto go into inner_loop_start
+    li s1, 0
+    
 inner_loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+   
+    bge s1, s3, inner_loop_end
+    
+    # Set a0 every inner loop
+    slli t0, s0, 2
+    mul t0, t0, s6
+    add a0, t0, s4
+    
+    # Set a2, a3, a4 every inner loop
+    mv a2, s6 # set the num 
+    li a3, 1 # set the stride of m1
+    mv a4, s3 # set the stride of m2
+    
+    # Set a1
+    slli t0, s1, 2
+    add a1, t0, s5
+    
+    # Set correct a6
+    addi s7, s7, 4 # move the result pointer
+    
+    # jump to dot function
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal dot
+    sw a0, 0(s7) # fill the result matrix
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    
+    addi s1, s1, 1 # add count of inner loop
+    j inner_loop_start
+    
+   
 inner_loop_end:
-
-
-
+    addi s0, s0, 1 # add count of outer loop
+    j outer_loop_start
 
 outer_loop_end:
 
-
     # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp) 
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp) 
+    lw s6, 24(sp)
+    lw s7, 28(sp)
+    addi sp, sp, 32
     jr ra
+    
+exception:
+    li a0 38
+    j exit
--- proj2/src/read_matrix.s
+++ proj2/src/read_matrix.s
@@ -27,16 +27,122 @@
 read_matrix:
 
     # Prologue
+    # save the callee's s
+    addi sp, sp, -24
+    sw s0, 0(sp) # for a0
+    sw s1, 4(sp) # for a1
+    sw s2, 8(sp) # for a2
+    sw s3, 12(sp) # for file descriptor
+    sw s4, 16(sp) # for result matirx pointer
+    sw s5, 20(sp) # for store the size(bytes)
+    
+    # save the arguments
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    
+    
+    # First fopen
+    addi sp, sp, -4
+    sw ra, 0(sp) # save the ra before calling fopen
+    # a0 stays the same
+    li a1, 0 # a1 == 0 -> only read
+    jal fopen # call fopen
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    li t0, -1 
+    beq a0, t0, efopen
+    mv s3, a0
+    
+    # Now s3 contains the file descriptor 
+    # Firse read the row
+    addi sp, sp, -4 
+    sw ra, 0(sp)
+    mv a0, s3
+    mv a1, s1
+    li a2, 4 # read 4 byte
+    jal fread # call fread
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    li t0, 4
+    bne a0, t0, efread
+ 
+    
 
+    
+    # Second read the column
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    mv a0, s3
+    mv a1, s2
+    li a2, 4
+    jal fread
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    li t0, 4
+    bne a0, t0, efread
+    
+# I don't think above is wrong.
 
-
-
-
-
-
-
-
+   
+    # Then call malloc to allocate memory
+    lw t0, 0(s1) # the row num
+    lw t1, 0(s2) # the column num
+    mul s5, t0, t1
+    slli s5, s5, 2 # s0 stores the size (byte)
+    mv a0, s5
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal malloc
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    beq a0, x0, emalloc
+    mv s4, a0
+ 
+    # Then call fread again to read the whole matrix
+    mv a0, s3 # a0 is the file descriptor
+    mv a1, s4 # a1 is the pointer of result memory
+    mv a2, s5 # a2 is the size(byte)
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal fread
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    bne s5, a0, efread
+    
+    # close the file
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    mv a0, s3 
+    jal fclose
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    bne a0, x0, efclose
+    
+    
     # Epilogue
-
-
+    mv a0, s4 # the result matrix pointer
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    addi sp, sp, 24
     jr ra
+    
+efopen:
+    li a0, 27
+    j exit
+    
+emalloc:
+    li a0, 26
+    j exit
+    
+efread:
+    li a0, 29
+    j exit
+    
+efclose:
+    li a0, 28
+    j exit
