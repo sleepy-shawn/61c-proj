cacc722580a934988ef14ff6dbcb1b4e75daadcc
ggshawn <geshuai@nwafu.edu.cn>
2024-11-28 22:45:14 +0800

    write matrix completedgit add .git add .

--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -25,16 +25,103 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -16
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    
+    mv s0, a0 # s0 is the pointer to string filename
+    mv s1, a1 # s1 is the pointer to the start of the matrix
+    mv s2, a2 # s2 is the row num
+    mv s3, a3 # s3 is the column num
+ 
 
 
-
-
-
-
-
+    # First open the file in write mode
+    # a0 don't need to change
+    li a1, 1 # write mode
+    
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    
+    jal fopen
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    
+    li t0, -1
+    beq a0, t0, exception_fopen
+    
+    mv s0, a0 # s0 is the file description
+    
+    
+    # store data of row num and column num in memory
+    addi sp, sp, -12
+    sw s2, 0(sp)
+    sw s3, 4(sp) 
+    sw ra, 8(sp)
+    
+    mv a0, s0 # a0 is the pointer to the string filename
+    add a1, sp, x0 # the pointer to data
+    li a2, 2 # the num of elements is 2
+    li a3, 4 # size of element
+    
+    jal fwrite
+    
+    lw s2, 0(sp)
+    lw s3, 4(sp)
+    lw ra, 8(sp)
+    addi sp, sp, 12
+    
+    li t0, 2
+    bne a0, t0, exception_fwrite
+    
+    # store data in file
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    
+    mv a0, s0 # the pointer to filename descriptor
+    mv a1, s1 # the data in memory
+    mul a2, s2, s3 # the num of elements
+    li a3, 4
+    
+    jal fwrite
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    mul t0, s2, s3
+    bne a0, t0, exception_fwrite
+    
+    
+    
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    mv a0, s0
+    
+    jal fclose
+    
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    bne a0, x0, exception_fclose
 
 
     # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    addi sp, sp, 16
     jr ra
+
+exception_fopen:
+    li a0, 27
+    j exit
+    
+exception_fwrite:
+    li a0, 30
+    j exit
+    
+exception_fclose:
+    li a0, 28
+    j exit
