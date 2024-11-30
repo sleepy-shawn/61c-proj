81802c4d0f0615f9f75b9075e25ca39e6f5c6edc
ggshawn <geshuai@nwafu.edu.cn>
2024-11-30 18:00:17 +0800

    proj2 finished

--- proj2/src/classify.s
+++ proj2/src/classify.s
@@ -32,60 +32,71 @@ classify:
     bne a0, t0, exception_arg 
     
 # save ra and s register
-    addi sp, sp, -32
-    sw s0, 0(sp)
-    sw s1, 4(sp)
-    sw s2, 8(sp)
-    sw s3, 12(sp)
-    sw s4, 16(sp)
-    sw s5, 20(sp)
-    sw s6, 24(sp)
-    sw s7, 28(sp)
+    addi sp, sp, -48
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
+    sw s7, 32(sp)
+    sw s8, 36(sp)
+    sw s9, 40(sp)
+    sw s10, 44(sp)
+  
+    mv s0, a1
+    mv s1, a2 # store the print argument
     
-    mv s0, ra
-    lw s1, 4(a1) # pointer to filepath string of m0
-    lw s2, 8(a1) # pointer to filepath string of m1
-    lw s3, 12(a1) # pointer to filepath string of input matrix
-    lw s4, 16(a1) # pointer to filepath string of output file
-    mv s5, a2 # store the print argument
+    
     # Read pretrained m0
-    mv a0, s1 
-    addi sp, sp, -8 # allocate memory for row num and column num
-    mv a1, sp
-    addi a2, sp, 4
-    
-    j read_matrix
-    
-    mv s1, a0 # s1 saves the pointer to m0
-    
-
-    # Read pretrained m1
-    mv a0, s2 
-    addi sp, sp, -8 # allocate memory for row num and column num
-    mv a1, sp
-    addi a2, sp, 4
-    
-    j read_matrix 
-    
-    mv s2, a0 # s2 saves the pointer to m1
-
-
-    # Read input matrix
-    mv a0, s3 
-    addi sp, sp, -8 # allocate memory for row num and column num
-    mv a1, sp
-    addi a2, sp, 4
+    li a0, 8
+    jal malloc
+    beq a0, x0, exception_malloc
+    mv s2, a0 # s2 stores m0's row and column
+    lw a0, 4(s0)
+    mv a1, s2
+    addi a2, s2, 4 
     
     jal read_matrix
     
-    mv s3, a0 # s3 saves the pointer to input matrix
+    mv s3, a0 # s3 saves the pointer to m0
+    
+
+    # Read pretrained m1
+    li a0, 8
+    jal malloc
+    beq a0, x0, exception_malloc
+    mv s4, a0 # s4 stores m1's row and column
+    lw a0, 8(s0)
+    mv a1, s4
+    addi a2, s4, 4 
+    
+    jal read_matrix 
+    
+    mv s5, a0 # s5 saves the pointer to m1
+
+
+    # Read input matrix
+    li a0, 8
+    jal malloc
+    beq a0, x0, exception_malloc
+    mv s6, a0 # s6 stores m0's row and column
+    lw a0, 12(s0)
+    mv a1, s6
+    addi a2, s6, 4 
+    
+    jal read_matrix
+    
+    mv s7, a0 # s7 saves the pointer to input matrix
 
 # ______________________________________________________________I don't think above is wrong!!!!!
     # Compute h = matmul(m0, input)
     
     # first let's malloc
-    lw t0, 16(sp)
-    lw t1, 4(sp)
+    lw t0, 0(s2)
+    lw t1, 4(s6)
     mul a0, t0, t1
     slli a0, a0, 2 # convert to bytes!!!
     
@@ -93,24 +104,24 @@ classify:
     
     beq a0, x0, exception_malloc
    
-    mv s6, a0 # s6 is now pointer of matrix h
+    mv s8, a0 # s8 is now pointer of matrix h
     
     # then set the arg for matmul
     
-    mv a0, s1
-    lw a1, 16(sp)
-    lw a2, 20(sp)
-    mv a3, s3
-    lw a4, 0(sp)
-    lw a5, 4(sp)
-    mv a6, s6 
+    mv a0, s3
+    lw a1, 0(s2)
+    lw a2, 4(s2)
+    mv a3, s7
+    lw a4, 0(s6)
+    lw a5, 4(s6)
+    mv a6, s8
     
     jal matmul
     
     # Compute h = relu(h)
-    mv a0, s6 
-    lw t0, 16(sp)
-    lw t1, 4(sp)
+    mv a0, s8 
+    lw t0, 0(s2)
+    lw t1, 4(s6)
     mul a1, t0, t1 # a1 is the size of t
     
     jal relu
@@ -118,8 +129,8 @@ classify:
     
     # Compute o = matmul(m1, h)    
     # first let's malloc again for matrix o
-    lw t0, 8(sp)
-    lw t1, 4(sp)
+    lw t0, 0(s4)
+    lw t1, 4(s6)
     mul a0, t0, t1
     slli a0, a0, 2 # Don't forget convert to bytes!
     
@@ -127,85 +138,94 @@ classify:
     
     beq a0, x0, exception_malloc
    
-    mv s7, a0 # s7 is now pointer of matrix o
+    mv s9, a0 # s9 is now pointer of matrix o
     
-    mv a0, s2
-    lw a1, 8(sp)
-    lw a2, 12(sp)
-    mv a3, s6
-    lw a4, 16(sp)
-    lw a5, 4(sp)
-    mv a6, s7
+    mv a0, s5
+    lw a1, 0(s4)
+    lw a2, 4(s4)
+    mv a3, s8
+    lw a4, 0(s2)
+    lw a5, 4(s6)
+    mv a6, s9
 
     jal matmul
     
-    mv a0, s6
-    jal free # h is no use any more
+    
     
 # _____________________________________________________I don't think above is wrong
     
     # Write output matrix o
-    mv a0, s4
-    mv a1, s7
-    lw a2, 8(sp)
-    lw a3, 4(sp)
+    lw a0, 16(s0)
+    mv a1, s9
+    lw a2, 0(s4)
+    lw a3, 4(s6)
     
     jal write_matrix
 
     # Compute and return argmax(o)
-    mv a0, s7
-    lw t0, 8(sp)
-    lw t1, 4(sp)
+    
+    mv a0, s9
+    lw t0, 0(s2)
+    lw t1, 4(s6)
     mul a1, t0, t1
     jal argmax
-    mv s6, a0 # s1 is the final result since it is no useful now
-    bne s5, x0, end # if s5 == x0, print result, if not end directly
-    
-print_argmax:
+    mv s10, a0 
+    bne s1, x0, no_print # if s1 == x0, print result, if not end directly
    
     # If enabled, print argmax(o) and newline
     # a0 don't need change
-    mv a0, s6
+    mv a0, s10
     jal print_int
     
     li a0, '\n'
     jal print_char
     
-    j end
+ 
     
 # Epilogue
     
-end:    
+no_print:    
     # free the three pointer
-    mv a0, s1
-    jal free
-    
     mv a0, s2
     jal free
     
     mv a0, s3
     jal free
     
+    mv a0, s4
+    jal free
+    
+    mv a0, s5
+    jal free 
+    
+    mv a0, s6
+    jal free
+    
     mv a0, s7
     jal free
     
-    # reset stack pointer
-    addi sp, sp, 24
+    mv a0, s8
+    jal free
+    
+    mv a0, s9
+    jal free
     
     # reset the ra and s registers
-    mv ra, s0
-    mv a0, s6
+    mv a0, s10
     
-    lw s0, 0(sp)
-    lw s1, 4(sp)
-    lw s2, 8(sp)
-    lw s3, 12(sp)
-    lw s4, 16(sp)
-    lw s5, 20(sp)
-    lw s6, 24(sp)
-    lw s7, 28(sp)
-    
-    addi sp, sp, 32
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    lw s7, 32(sp)
+    lw s8, 36(sp)
+    lw s9, 40(sp)
+    lw s10, 44(sp)
+    addi sp, sp, 48
     
     jr ra
     
